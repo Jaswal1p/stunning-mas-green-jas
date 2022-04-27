ddc1906705f1baad0504e43b339ad1a522d6524a
moon9588 <moon9588@yahoo.com>
2022-04-26 20:47:59 -0400

    added userSchema, const user

--- server/models/Users.js
+++ server/models/Users.js
@@ -44,3 +44,28 @@ const userSchema = new Schema(
   },
 
   // Now this needs to be set to be used in the following virtual
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
