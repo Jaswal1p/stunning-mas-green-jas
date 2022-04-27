0cf842ea791ac7ee3f249163fe01b4390d8404d6
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-26 20:43:15 -0400

    additional code added to server.js

--- server/server.js
+++ server/server.js
@@ -23,3 +23,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Now listening on localhost:${PORT}`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+});
