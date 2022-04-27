09b4685cf6aa75f7be267a7cc2594fb40b66d5b2
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-26 20:38:15 -0400

    declared consts in the server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
+const path = require('path');
 const express = require('express');
+const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+});
+
