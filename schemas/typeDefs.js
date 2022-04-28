39de4e71eec031766b9f6939b9f21859d46e5c7b
Zack Greenfield <zackg007@verizon.net>
2022-04-28 19:21:13 -0400

    finished typeDefs file

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -30,4 +30,34 @@ const typeDefs = gql`
         likedMovies: [Movie]
     }
 
-    
+    type Query {
+        me: User
+        movies: [Movie]
+        movie(movieId: ID!): Movie
+        users: [User]
+        user(username: String!): User
+    }
+
+    input MovieInput {
+        externalMovieId: Int
+        rating: Float
+        voteCount: Int
+        title: String
+        overview: String
+        releaseDate: String
+        poster: String
+        trailer: String
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addFriend(friendId: ID!): User
+        addMovie(input: MovieInput!): Movie
+        likeMovie(movieId: ID!): User
+        dislikeMovie(movieId: ID!): User
+    }
+
+`;
+
+module.exports = typeDefs;
