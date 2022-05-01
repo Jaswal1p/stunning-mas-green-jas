7489dfae8074e243d0a7904ab88b22e72c207cb2
Jaswal1p <Jaswal1p@yahoo.com>
2022-05-01 15:06:10 -0400

    added declarations in signupForm.js, app.js + added httplink, authlink & npm i @apollo/client graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+// import logo from './logo.svg';
+// import './App.css';
+
+import { setContext } from '@apollo/client/link/context';
+
+// import  ApolloClient  from 'apollo-boost';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import SavedMovies from './pages/SavedMovies';
+import SearhMovies from './pages/SearchMovies';
+
+
+
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+     headers: {
+         ...headers,
+         authorization: token ? `Bearer ${token}` : '',
+         },
+  
+     };
+  });
+
+const client = new ApolloClient({
+  
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+
+
+});
 
 function App() {
   return (
