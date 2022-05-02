6c111da61162fb97022ccac125f8399994c58865
moon9588 <moon9588@yahoo.com>
2022-05-02 12:16:03 -0400

    added saved movie stoarage

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,6 +1,16 @@
 export const getSavedMovieIds = () => {
     const savedMovieIds = localStorage.getItem('saved_movie')
-    ? JSON.parse(localStorage.getItem)('saved_movie'))
-    : [];
+      ? JSON.parse(localStorage.getItem('saved_movie'))
+      : [];
+    return savedMovieIds;
+  };
 
-}
+  export const savedMovieIds = (bookIdArr) => {
+      if (bookIdArr.length){
+          localStorage.setItem('saved_movie', JSON.stringify(movieIdArr));
+      } else {
+          localStorage.removeItem('saved_books')      
+      }
+  };
+
+  
