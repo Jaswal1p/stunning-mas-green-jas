46caf20e3e128a2d2c32240d13b3c277af6a3b95
moon9588 <moon9588@yahoo.com>
2022-05-02 11:47:37 -0400

    added email part signupform

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -16,7 +16,52 @@ const SignupForm = () => {
         const {name, value} = event.target;
         setUserFormData({...userFormData, [name]: value});
     };
-    const handleFormsubmit = async (event) => {
-        event.preventDefault();
+    const handleFormSubmit = async (event) =>{
+        event.preventDefault();   
     }
-}
+    try {
+        const { data} = await addUser({
+            variables:{...userFormData}
+        });
+        Auth.login(data.addUser.Token);
+    } catch (e){
+        console.error(e);
+        setShowAlert(true);
+    }
+    setUserFormData({
+        username: '',
+        email: '',
+        password:'',
+    });
+};
+
+return (
+    <>
+    {}
+    <form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Something went wrong with your signup!
+        </Alert>
+        <Form.Group>
+            <Form htmlFor='username'>Username</Form>
+            <Form.Control
+            type='text'
+            placeholder='username'
+            onChange={hangleInputChange}
+            value={userFormData.username}
+            required />
+        <Form.Control.Feedback type='invalid'>Username is required</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Label htmlFor='email'>Email</Form.Label>
+        <Form.Control
+            type='email'
+            placeholder='Your email address'
+            name='email'
+            onChange={handleInputChagne}
+            value={userFormData.email}
+            required />
+        <Form.Group>
+            <Form.Control.Feedback type='invalid'>Email is required</Form.Control.Feedback>
+        </Form.Group>
+    </>
+)
