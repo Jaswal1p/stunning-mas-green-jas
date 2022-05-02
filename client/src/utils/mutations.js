d0ec70bd57ccc72d68f9d1fe775fd0a4fd49bc4e
Zack Greenfield <zackg007@verizon.net>
2022-05-02 09:10:09 -0400

    added first mutation to file

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag';
+
+export const ADD_USER =gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+                user {
+                    _id
+                    username
+                    email
+                    friendCount
+                    likedMovies {
+                        _id
+                        externalMovieId
+                        rating
+                        voteCount
+                        title
+                        overview
+                        releaseDate
+                        poster
+                        trailer
+                    }
+                }
+        }
+    }
+`;
