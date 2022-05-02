5b09bf83395d97e10ef01a412d9b7274e3d9ff8d
Jaswal1p <Jaswal1p@yahoo.com>
2022-05-02 18:15:50 -0400

    corrections in the localstorage.js

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,21 +1,21 @@
 export const getSavedMovieIds = () => {
-    const savedMovieIds = localStorage.getItem('saved_movie')
-      ? JSON.parse(localStorage.getItem('saved_movie'))
+    const savedMovieIds = localStorage.getItem('saved_movies')
+      ? JSON.parse(localStorage.getItem('saved_movies'))
       : [];
     return savedMovieIds;
   };
 
-  export const savedMovieIds = (movieIdArr) => {
+  export const saveMovieIds = (movieIdArr) => {
       if (movieIdArr.length){
-          localStorage.setItem('saved_movie', JSON.stringify(movieIdArr));
+          localStorage.setItem('saved_movies', JSON.stringify(movieIdArr));
       } else {
           localStorage.removeItem('saved_movies')      
       }
   };
 
   export const removeMovieId = (movieId) =>{
-      const savedMovieIds= localStorage.getItem('saved_movie')
-      ? JSON.parse(localStorage.getItem('saved_movie'))
+      const savedMovieIds= localStorage.getItem('saved_movies')
+      ? JSON.parse(localStorage.getItem('saved_movies'))
       : null;
 
       if(!savedMovieIds){
@@ -23,7 +23,9 @@ export const getSavedMovieIds = () => {
       }
       const updatedSavedMovieIds = savedMovieIds?.filter((savedMovieId)=> savedMovieId !==movieId);
       localStorage.setItem('saved_movies', JSON.stringify(updatedSavedMovieIds));
-  }
+
+      return true;
+};
 
 
 
