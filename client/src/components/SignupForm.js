b6b2e5c4fee25a41edd0e1385c92b1beb5f71318
moon9588 <moon9588@yahoo.com>
2022-05-01 23:58:24 -0400

    worked on signup form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -6,3 +6,17 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { ADD_USER } from '../utils/mutations';
 
 import { useMutation } from '@apollo/client';
+
+const SignupForm = () => {
+    const [addUser, {error}] = useMutation76(ADD_USER);
+    const [userFormData, setUserFormData] = useState({ username:'', email:'', password:''});
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const handleInputChagne = (event) => {
+        const {name, value} = event.target;
+        setUserFormData({...userFormData, [name]: value});
+    };
+    const handleFormsubmit = async (event) => {
+        event.preventDefault();
+    }
+}
